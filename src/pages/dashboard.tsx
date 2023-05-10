56b0b5994e44795d97f5f68ca80e383dd4a6093f
mitro-ubaidillah <ubaidillahmitro@gmail.com>
2023-05-10 21:12:23 +0700

    initial project fake store

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import { Box, Stack } from '@mui/material';
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <React.Fragment>
+        <Stack>
+            <h1>Fake Store Coming Soon</h1>
+        </Stack>
+    </React.Fragment>
+  )
+}
+
+export default Dashboard;
